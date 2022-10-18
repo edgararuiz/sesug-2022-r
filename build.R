860fd739858efd5d87680bff52e2f8906c5952d6
Edgar Ruiz <edgararuiz@gmail.com>
2022-10-18 08:26:59 -0500

    Fixes indes page for slides, adds dplyr slides

--- build.R
+++ build.R
@@ -21,7 +21,7 @@ c("setup.Rmd", "slides.css", "xaringan-themer.css", "xaringan-themer.R", "dsbox-
   })
 
 
-c("u1-d01-welcome", "u2-d02-ggplot2") %>% 
+c("u1-d01-welcome", "u2-d02-ggplot2", "u2-d06-grammar-wrangle") %>% 
 #c("u1-d01-welcome") %>% 
   map(~{
     try(
@@ -31,8 +31,9 @@ c("u1-d01-welcome", "u2-d02-ggplot2") %>%
     if(dir_exists(files_folder)) dir_delete(files_folder)
     html_file <- path(slides_folder, .x, .x, ext = "html")
     if(file_exists(html_file)) file_delete(html_file)
-    print(html_file)
   })
+
+file_copy("slides.Rmd", slides_folder)
 
 dir_info(slides_folder, recurse = 3, glob = "*.Rmd") %>% 
   filter(!str_detect(path, "setup")) %>%
@@ -40,9 +41,8 @@ dir_info(slides_folder, recurse = 3, glob = "*.Rmd") %>%
   walk(render, output_file = "index.html")
 
 docs_slides <- path("docs/slides")
-if(dir_exists(docs_slides)) {
-  dir_delete(docs_slides)
-  dir_copy("slides", docs_slides)
-}
+if(dir_exists(docs_slides)) dir_delete(docs_slides)
+dir_copy(slides_folder, docs_slides)
 
-render("slides.Rmd", output_dir = "slides", output_file = "index.html")
+
+
