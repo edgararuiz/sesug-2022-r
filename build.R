11133bb5db42e0d87dddadcaa562f5440181fade
Edgar Ruiz <edgararuiz@gmail.com>
2022-10-18 11:58:15 -0500

    Adds seven additional section slides

--- build.R
+++ build.R
@@ -20,9 +20,10 @@ c("setup.Rmd", "slides.css", "xaringan-themer.css", "xaringan-themer.R", "dsbox-
     )
   })
 
-
-c("u1-d01-welcome", "u2-d02-ggplot2", "u2-d06-grammar-wrangle") %>% 
-#c("u1-d01-welcome") %>% 
+c("u1-d01-welcome", "u2-d02-ggplot2", "u2-d06-grammar-wrangle", "u2-d12-data-import",
+  "u2-d13-data-recode", "u2-d14-effective-dataviz", "u2-d18-web-scrape", "u2-d21-functions",
+  "u2-d22-iteration", "u5-d03-interactive-web-app") %>% 
+  head(1) %>% 
   map(~{
     try(
       dir_copy(path(source_slides, .x), path(slides_folder))
@@ -33,12 +34,20 @@ c("u1-d01-welcome", "u2-d02-ggplot2", "u2-d06-grammar-wrangle") %>%
     if(file_exists(html_file)) file_delete(html_file)
   })
 
-file_copy("slides.Rmd", slides_folder)
+file_copy("slides.Rmd", slides_folder, overwrite = TRUE)
 
-dir_info(slides_folder, recurse = 3, glob = "*.Rmd") %>% 
+dir_info(slides_folder, recurse = 3, glob = "*.Rmd") %>%  
   filter(!str_detect(path, "setup")) %>%
+  #head(1) %>% 
   pull(path) %>% 
-  walk(render, output_file = "index.html")
+  walk(~{
+    temp_file <- path(tempfile(), ext = "R")
+    writeLines(
+      paste0("rmarkdown::render('", path_abs(.x),"', output_file = 'index.html')"), 
+      con = temp_file
+        )
+    rstudioapi::jobRunScript(temp_file, name = .x)
+  })
 
 docs_slides <- path("docs/slides")
 if(dir_exists(docs_slides)) dir_delete(docs_slides)
